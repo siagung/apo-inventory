b9f113d15c1c53637e4d530f481af92278e412fc
Kevin Panuelos <kmp091@gmail.com>
2011-11-09 23:04:06 +0800

    Product facilitator class
    This represents the Product table in the database.
    
    Signed-off-by: Kevin Panuelos <kmp091@gmail.com>

---
+++ src/com/apo/facilitator/ProductFacilitator.java
@@ -0,0 +1,34 @@
+package com.apo.facilitator;
+
+import javax.swing.ComboBoxModel;
+import javax.swing.ListModel;
+import javax.swing.table.TableModel;
+
+import com.apo.operator.RevisableDBOperator;
+
+public class ProductFacilitator extends Facilitator {
+
+	public ProductFacilitator(RevisableDBOperator operator) {
+		super(operator);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public ListModel getListModel(String filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public TableModel getTableModel(String filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ComboBoxModel getComboBoxModel(String filter) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
