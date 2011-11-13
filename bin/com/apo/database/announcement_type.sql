b040aec6206931a8ee665c57abbd55df579a6284
Kevin Panuelos <kmp091@gmail.com>
2011-11-13 11:30:03 +0800

    Scaled resources
    50x50 PNG icons
    
    Signed-off-by: Kevin Panuelos <kmp091@gmail.com>

---
+++ bin/com/apo/database/announcement_type.sql
@@ -0,0 +1,11 @@
+/*
+-- Query: select * from `apo_db`.`announcement_type`
+LIMIT 0, 1000
+
+-- Date: 2011-11-12 16:21
+*/
+INSERT INTO `announcement_type` (`announcement_type_id`,`announcement_type_name`) VALUES (1,'Custom');
+INSERT INTO `announcement_type` (`announcement_type_id`,`announcement_type_name`) VALUES (2,'Added');
+INSERT INTO `announcement_type` (`announcement_type_id`,`announcement_type_name`) VALUES (3,'Edited');
+INSERT INTO `announcement_type` (`announcement_type_id`,`announcement_type_name`) VALUES (4,'Reverted');
+INSERT INTO `announcement_type` (`announcement_type_id`,`announcement_type_name`) VALUES (5,'Deleted');
---
+++ bin/com/apo/database/privileges.sql
@@ -0,0 +1,48 @@
+/*
+-- Query: select * from `apo_db`.`privilege`
+LIMIT 0, 1000
+
+-- Date: 2011-11-12 15:51
+*/
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (1,'Create employees');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (2,'Update employees');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (3,'Delete employees');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (4,'Restore employees');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (5,'Promote and demote employees');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (6,'View employees');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (7,'Create products');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (8,'Update products');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (9,'Revert products');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (10,'Delete products');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (11,'View products');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (12,'Create product categories');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (13,'Update product categories');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (14,'Delete product categories');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (15,'View product categories');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (16,'Create suppliers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (17,'Update suppliers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (18,'Restore suppliers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (19,'Delete suppliers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (20,'View suppliers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (21,'View supplier products');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (22,'Create customers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (23,'Delete customers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (24,'Restore customers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (25,'View customers');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (26,'Add customer visits');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (27,'Modify customer visits');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (28,'Delete customer visits');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (29,'Create orders');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (30,'Modify orders');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (31,'View orders');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (32,'Delete orders');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (33,'Restore orders');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (34,'Add payments');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (35,'Edit payments');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (36,'Remove payments');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (37,'Read announcements');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (38,'Post announcements');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (39,'View customer order history');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (40,'View sales report');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (41,'View product purchases history');
+INSERT INTO `privilege` (`privilege_id`,`privilege_desc`) VALUES (42,'View stock report');
